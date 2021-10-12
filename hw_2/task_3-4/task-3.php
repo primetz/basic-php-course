666512810b68f741fa5f48b9f2920a490d841508
Stanislav Rodikov <stanislav.rodikov@yahoo.com>
2021-10-12 19:21:37 +0300

    Practical work 2

---
+++ hw_2/task_3-4/task-3.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * 3. Реализовать основные 4 арифметические операции в виде функций с двумя параметрами.
+ *    Обязательно использовать оператор return.
+*/
+function sum($x, $y) {
+    return $x + $y;
+}
+
+function diff($x, $y) {
+    return $x - $y;
+}
+
+function mult($x, $y) {
+    return $x * $y;
+}
+
+function div($x, $y) {
+    return $x / $y;
+}
